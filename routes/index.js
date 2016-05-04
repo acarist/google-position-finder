88093292e5d727ef00d650e4fd78b496e91ada2a
Ramazan Acar <acarist@gmail.com>
2016-05-04 07:13:34 +0000

    post and partial

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,43 @@ var google = require('google')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	google.resultsPerPage = 25
-	var nextCounter = 0
+	res.render('index');
+});
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
+	google.resultsPerPage = 10;
+	// google.lang = 'tr'
+	// google.tld = 'tr'
+	// google.nextText = 'Sonraki'
+	var nextCounter = 0;
 	var result = [];
-	google('node.js best practices', function (err, resp){
-	  if (err) console.error(err)
-	  	//console.log(res.links);
-	 	result = resp.links;
-	 	res.render('index', {"searchResult":null});
+	google(req.body.keyword, function (err, resp){
+		if (err) console.error(err)
+
+	  for (var i = 0; i < resp.links.length; ++i) {
+			result.push(resp.links[i])
+	    var link = resp.links[i];
+	    console.log(link.title + ' - ' + link.href)
+	    console.log(link.description + "\n")
+	  }
+
+	  if (nextCounter < req.body.range) {
+	    nextCounter += 1
+	    if (res.next) res.next()
+	  } else {
+			res.render('index', {"searchResult":result});
+	  }
 	});
-  
+
 });
 
 module.exports = router;
+
+
+var resultSample = {
+		title: 'Leadergamer - Oyun haberleri, Oyun incelemeleri',
+    link: 'http://leadergamer.com.tr/',
+    description: 'Oyun incelemeleri, haberler, video ve fotoğraf galerileri yer almaktadır.',
+    href: 'http://leadergamer.com.tr/'
+	};
